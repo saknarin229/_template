7e3db3bb4139bc6cfaf317f36eea3ce811c5712a
saknarin229 <saknarin229@gmail.com>
2022-04-24 22:06:01 +0700

    ไฟล์ดาต้าเบสอยู่นี้

--- dbconnect/connect.php
+++ dbconnect/connect.php
@@ -5,8 +5,8 @@ class dbconnect{
     static private function conn(){
         $servername = "localhost";
         $username = "root";
-        $password = "";
-        $db = 'product_data';
+        $password = ""; //123456789
+        $db = 'product_data'; //database name
 
         $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
@@ -16,13 +16,13 @@ class dbconnect{
     //รูปแบบการเรียกใช้งาน!
     //sql * form where ?,?
     //array('data','data')
-    static public function ExecuteData($sql, $data){
+    static public function ExecuteData($sql, $data){ //insert update ข้อมูลใช้ตัวนี้
         $sth = self::conn()->prepare($sql);
         $sth->execute($data);
         return true;
     }
 
-    static public function getExecute($sql, $data){
+    static public function getExecute($sql, $data){ //ดึงข้อมูล ใช้ตัวนี้
         $sth = self::conn()->prepare($sql);
         $sth->execute($data);
         return $sth->fetchAll(PDO::FETCH_ASSOC);
