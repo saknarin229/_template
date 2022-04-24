414911c680dfdb52fd4dd73cc6067adb63beeaa3
saknarin229 <saknarin229@gmail.com>
2022-04-24 14:17:33 +0700

    first commit

--- action/addproduct.class.php
+++ action/addproduct.class.php
@@ -6,11 +6,11 @@ class addproductClass {
     }
 
     static private function insertData(){
-
+        $sql = "INSERT INTO product_data(productName, productDetail, productPrice, productCode, productStatus) VALUES (?,?,?,?,?)";
     }
 
     static private function updateData(){
-
+        $sql = "UPDATE product_data SET productName=?,productDetail=?,productPrice=?,productCode=?,productStatus=? WHERE id=?";
     }
 
     static public function getData(){
