5197b00c1a9a40d0bcf7549138373136bc0110ea
saknarin229 <saknarin229@gmail.com>
2022-04-24 13:59:33 +0700

    main

---
+++ action/addproduct.class.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'dbconnect/connect.php';
+class addproductClass {
+    static public function action(){
+
+    }
+
+    static private function insertData(){
+
+    }
+
+    static private function updateData(){
+
+    }
+
+    static public function getData(){
+
+    }
+}
